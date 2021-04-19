8d9a4b41be743d032d6a7693e30344f400551fe7
SB <sb@SARARATs-MacBook-Pro.local>
2021-04-19 02:59:40 -0400

    created weather(Boston) status icon

---
+++ script.js
@@ -0,0 +1,19 @@
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Boston&units=imperial&appid=7d3357f65684a621b095667f5ebc5166",
+
+function(data){
+            console.log(data);
+
+            var icon = "http://openweathermap.org/img/wn/" + 
+            data.weather[0].icon + ".png";
+
+            var temp =  Math.floor(data.main.temp) + "°F";
+            
+            var weather = data.weather[0].main;
+
+            $(".icon").attr("src", icon);
+            $(".weather").append(weather);
+            $(".temp").append(temp);
+
+      });
+
+
