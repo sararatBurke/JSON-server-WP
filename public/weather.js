d16c6ce669cbddbf35b6914de300f3badfdde952
SB <sb@SARARATs-MacBook-Pro.local>
2021-04-21 20:41:34 -0400

    added high-low temp, styled

--- public/weather.js
+++ public/weather.js
@@ -1,4 +1,4 @@
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Boston&units=imperial&appid=7d3357f65684a621b095667f5ebc5166",
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Bangkok&units=imperial&appid=7d3357f65684a621b095667f5ebc5166",
 
 function(data){
             console.log(data);
@@ -6,7 +6,10 @@ function(data){
             var icon = "http://openweathermap.org/img/wn/" + 
             data.weather[0].icon + ".png";
 
-            var temp =  Math.floor(data.main.temp) + "°F";
+            var temp =  Math.floor(data.main.temp) + '°F';
+
+            var h = 'H:' + Math.floor(data.main.temp_max) + '°';
+            var l = 'L:' + Math.floor(data.main.temp_min) + '°';
             
             var weather = data.weather[0].main;
             var city = data.name;
@@ -16,6 +19,8 @@ function(data){
             $(".weather").append(weather);
             $(".temp").append(temp);
 
+            $(".hTemp").append(h);
+            $(".lTemp").append(l);
       });
 
 
